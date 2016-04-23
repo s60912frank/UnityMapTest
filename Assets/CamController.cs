2934cc40dc8a1a6583eb2154e9b2a90387f390cf
s60912frank <s60912frank@gmail.com>
2016-04-23 17:25:34 +0800

    可移動地圖了大概，程式碼超臭

---
+++ Assets/CamController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour {
+    private Transform trans;
+    private float startX;
+    private float startY;
+    private GameObject map;
+    private Vector3 dragOrigin;
+    private float dragSpeed = 0.5f;
+	// Use this for initialization
+	void Start () {
+        trans = gameObject.transform;
+        startX = trans.position.x;
+        startY = trans.position.y;
+        map = GameObject.Find("Plane");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            trans.Translate(Vector3.up * 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            trans.Translate(Vector3.down * 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            trans.Translate(Vector3.left * 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            trans.Translate(Vector3.right * 0.5f);
+        }
+        //if(trans.position.y > MapProcessor.mapBoundaries[0] ||
+        //    trans.position.y < MapProcessor.mapBoundaries[1] ||
+        //    trans.position.x < MapProcessor.mapBoundaries[2] ||
+        //    trans.position.x > MapProcessor.mapBoundaries[3])
+        //{
+        //    //Debug.Log("OUT!!!!");
+        //}
+
+        if (trans.position.x - startX > 9)
+        {
+            Debug.Log(trans.position.x - startX);
+            startX = trans.position.x;
+            //xtile+1
+            map.BroadcastMessage("GetNewTile", new int[] { 1, 0 });
+            
+        }
+        else if (trans.position.x - startX < -9)
+        {
+            //xtile-1
+            startX = trans.position.x;
+            map.BroadcastMessage("GetNewTile", new int[] { -1, 0 });
+        }
+
+        if (trans.position.y - startY > 9)
+        {
+            //ytile+1
+            startY = trans.position.y;
+            map.BroadcastMessage("GetNewTile", new int[] { 0, 1 });
+        }
+        else if (trans.position.y - startY < -9)
+        {
+            //ytile-1
+            startY = trans.position.y;
+            map.BroadcastMessage("GetNewTile", new int[] { 0, -1 });
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(-pos.x * dragSpeed, -pos.y * dragSpeed, 0);
+        transform.Translate(move, Space.World);  
+	}
+}
--- Assets/MapProcessor.cs
+++ Assets/MapProcessor.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 public class MapProcessor : MonoBehaviour {
     public const string API_KEY = "vector-tiles-vxQ7SnN";
     private const string MAP_TYPE = "boundaries,buildings,roads";
+    public static float[] mapBoundaries = new float[] { 0, 0, 0, 0 };
+    private int xTile;
+    private int yTile;
+    private List<int[]> drewTile;
     public string url;
     private float latitude = 25.0488153f;
     private float longtitude = 121.445669f;
@@ -12,23 +16,53 @@ public class MapProcessor : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         int[] tile = WorldToTilePos(longtitude, latitude, zoom);
-        url = "https://vector.mapzen.com/osm/" + MAP_TYPE + "/" + zoom + "/" + tile[0] + "/" + tile[1] + ".json?api_key=" + API_KEY;
-        Debug.Log(url);
-        WWW request = new WWW(url);
-        StartCoroutine(WaitForRequest(request));
+        xTile = tile[0];
+        yTile = tile[1];
+        requestMap(tile[0], tile[1]);
+        drewTile = new List<int[]>();
+        drewTile.Add(new int[] { tile[0], tile[1] });
+        //requestMap(tile[0] + 1, tile[1]);
+        //requestMap(tile[0] - 1, tile[1]);
+        //requestMap(tile[0], tile[1] + 1);
+        //requestMap(tile[0], tile[1] - 1);
+        //requestMap(tile[0] + 1, tile[1] + 1);
+        //requestMap(tile[0] + 1, tile[1] - 1);
+        //requestMap(tile[0] - 1, tile[1] + 1);
+        //requestMap(tile[0] - 1, tile[1] - 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
 
     public int[] WorldToTilePos(float lon, float lat, int zoom)
     {
-        int worldX = (int)((lon + 180.0) / 360.0 * (1 << zoom));
-        int worldY = (int)((1.0f - Mathf.Log(Mathf.Tan(lat * Mathf.PI / 180.0f) + 1.0f / Mathf.Cos(lat * Mathf.PI / 180.0f)) / Mathf.PI) / 2.0f * (1 << zoom));
-
+        int worldX = (int)Mathf.Floor((lon + 180.0f) / 360.0f * (1 << zoom));
+        int worldY = (int)Mathf.Floor(((1.0f - Mathf.Log(Mathf.Tan(lat * Mathf.PI / 180.0f) + 1.0f / Mathf.Cos(lat * Mathf.PI / 180.0f)) / Mathf.PI) / 2.0f * (1 << zoom)));
+        float[] latlon = TileToWorldPos(worldX, worldY, zoom);
+        //tilePlane.transform.position = new Vector3(latlon[0] - latitude, latlon[1] - latitude, 0);
+        longtitude = latlon[0];
+        latitude = latlon[1];
         return new int[] { worldX, worldY };
+    }
+
+    public float[] TileToWorldPos(float tile_x, float tile_y, int zoom)
+    {
+        float n = Mathf.PI - ((2.0f * Mathf.PI * tile_y) / Mathf.Pow(2.0f, zoom));
+
+        float x = (float)((tile_x / Mathf.Pow(2.0f, zoom) * 360.0) - 180.0);
+        float y = (float)(180.0 / Mathf.PI * Mathf.Atan((Mathf.Exp(n) - Mathf.Exp(-n)) / 2));
+        //Debug.Log("x:" + x + "y:" + y);
+        return new float[] { x, y };
+    }
+
+    private void requestMap(int xtile, int ytile)
+    {
+        url = "https://vector.mapzen.com/osm/" + MAP_TYPE + "/" + zoom + "/" + xtile + "/" + ytile + ".json?api_key=" + API_KEY;
+        Debug.Log(url);
+        WWW request = new WWW(url);
+        StartCoroutine(WaitForRequest(request));
     }
 
     IEnumerator WaitForRequest(WWW www)
@@ -48,12 +82,15 @@ public class MapProcessor : MonoBehaviour {
 
     private void JsonProssor(string rawData)
     {
+        
+
         JSONObject data = new JSONObject(rawData);
         JSONObject buildings = data["buildings"]["features"];
         foreach (JSONObject obj in buildings.list)
         {
             List<Vector2> coords = new List<Vector2>();
             string type = obj["geometry"]["type"].ToString().Replace("\"", "");
+            //Debug.Log(type);
             switch (type)
             {
                 case "Point":
@@ -62,99 +99,183 @@ public class MapProcessor : MonoBehaviour {
                 case "Polygon":
                     foreach (JSONObject co in obj["geometry"]["coordinates"].list[0].list)
                     {
-                        //Debug.Log(co);
-                        float worldX = (float.Parse(co[0].ToString()) - longtitude) * 1000f;
-                        float worldY = (float.Parse(co[1].ToString()) - latitude) * 1000f;
-                        //Vector2 vec = new Vector2(float.Parse(co[0].ToString()), float.Parse(co[1].ToString()));
-                        coords.Add(new Vector2(worldX, worldY));
-                        Debug.Log("X:" + worldX + "Y:" + worldY);
-                        
+                        float[] world = coordTrans(float.Parse(co[0].ToString()), float.Parse(co[1].ToString()));
+                        coords.Add(new Vector3(world[0], world[1], 0));                      
                     }
-                    Debug.Log(obj["geometry"]["coordinates"]);
                     break;
-                case "LineString":
-                    break;
-                case "MultiLineString":
+                default:
+                    Debug.Log(type);
                     break;
             }
-            DrawMapObj(coords.ToArray());
+            DrawBuildings(type, coords.ToArray());
         }
 
+        JSONObject roads = data["roads"]["features"];
+        foreach (JSONObject obj in roads.list)
+        {
+            List<Vector3> coords = new List<Vector3>();
+            string type = obj["geometry"]["type"].ToString().Replace("\"", "");
+            switch (type)
+            {
+                case "LineString":
+                    foreach (JSONObject co in obj["geometry"]["coordinates"].list)
+                    {
+                        float[] world = coordTrans(float.Parse(co[0].ToString()), float.Parse(co[1].ToString()));
+                        coords.Add(new Vector3(world[0], world[1], -0.1f));
+                    }
+                    break;
+                case "MultiLineString":
+                    foreach (JSONObject co in obj["geometry"]["coordinates"].list)
+                    {
+                        List<Vector3> smallLine = new List<Vector3>();
+                        foreach (JSONObject co2 in co.list)
+                        {
+                            float[] world = coordTrans(float.Parse(co2[0].ToString()), float.Parse(co2[1].ToString()));
+                            smallLine.Add(new Vector3(world[0], world[1], -0.1f));
+                        }
+                        DrawRoads(smallLine.ToArray());
+                    }
+                    break;
+                default:
+                    Debug.Log(type);
+                    break;
+            }
+            DrawRoads(coords.ToArray());
+        }
 
+        JSONObject boundaries = data["boundaries"]["features"];
+        foreach (JSONObject obj in boundaries.list)
+        {
+            List<Vector3> coords = new List<Vector3>();
+            string type = obj["geometry"]["type"].ToString().Replace("\"", "");
+            switch (type)
+            {
+                case "LineString":
+                    foreach (JSONObject co in obj["geometry"]["coordinates"].list)
+                    {
+                        float[] world = coordTrans(float.Parse(co[0].ToString()), float.Parse(co[1].ToString()));
+                        coords.Add(new Vector3(world[0], world[1], 0));
+                    }
+                    break;
+                case "MultiLineString":
+                    foreach (JSONObject co in obj["geometry"]["coordinates"].list)
+                    {
+                        List<Vector3> smallLine = new List<Vector3>();
+                        foreach (JSONObject co2 in co.list)
+                        {
+                            float[] world = coordTrans(float.Parse(co2[0].ToString()), float.Parse(co2[1].ToString()));
+                            smallLine.Add(new Vector3(world[0], world[1], 0));
+                        }
+                        DrawRoads(smallLine.ToArray());
+                    }
+                    break;
+                default:
+                    Debug.Log(type);
+                    break;
+            }
+            DrawRoads(coords.ToArray());
+        }
     }
 
-    private void DrawMapObj(Vector2[] vertices2D)
+    private void DrawBuildings(string type, Vector2[] vertices2D)
     {
-        //Debug.Log(vecs);
-        //Vector2[] vertices2D = new Vector2[] {
-        //    new Vector2(0,0),
-        //    new Vector2(0,50),
-        //    new Vector2(50,50),
-        //    new Vector2(50,100),
-        //    new Vector2(0,100),
-        //    new Vector2(0,150),
-        //    new Vector2(150,150),
-        //    new Vector2(150,100),
-        //    new Vector2(100,100),
-        //    new Vector2(100,50),
-        //    new Vector2(150,50),
-        //    new Vector2(150,0),
-        //};
-
-        // Use the triangulator to get indices for creating triangles
-        //Vector2[] vertices2D = vecs;
-        //GameObject obj = new GameObject();
-        ////Debug.Log(coords);
-        //Triangulator tr = new Triangulator(vertices2D);
-        //int[] indices = tr.Triangulate();
-        //int[] rindices = new int[indices.Length];
-        //for (int i = 0; i < indices.Length; i++)
-        //{
-        //    rindices[indices.Length - i - 1] = indices[i];
-        //}
-        //// Create the Vector3 vertices
-        //Vector3[] vertices = new Vector3[vertices2D.Length];
-        //for (int i = vertices.Length - 1; i >= 0; i--)
-        //{
-        //    vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
-        //    //Debug.Log("X:" + vertices2D[i].x + "Y:" + vertices2D[i].y);
-        //}
-
-        //List<Vector3> vec3 = new List<Vector3>();
-        //foreach (Vector2 vec in coords)
-        //{
-        //    vec3.Add(new Vector3(vec.x, vec.y, 0));
-        //}
-
-
-        // Use the triangulator to get indices for creating triangles
-        Triangulator tr = new Triangulator(vertices2D);
-        int[] indices = tr.Triangulate();
-
-        // Create the Vector3 vertices
         Vector3[] vertices = new Vector3[vertices2D.Length];
         for (int i = 0; i < vertices.Length; i++)
         {
-            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
+            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, -0.1f);
+            setBoundaries(vertices[i].x, vertices[i].y);
         }
-        //Create the mesh
-        //Mesh msh = gameObject.GetComponent<MeshFilter>().mesh;
-        Mesh msh = new Mesh();
-        msh.Clear();
-        msh.vertices = vertices;
-        //msh.triangles = rindices;
-        msh.triangles = indices;
-        msh.RecalculateNormals();
-        msh.RecalculateBounds();
 
-        // Set up game object with mesh;
+        switch (type)
+        {
+            case "Polygon":
+                GameObject obj = new GameObject();
+                // Use the triangulator to get indices for creating triangles
+                Triangulator tr = new Triangulator(vertices2D);
+                int[] indices = tr.Triangulate();
+                //Create the mesh
+                Mesh msh = new Mesh();
+                msh.Clear();
+                msh.vertices = vertices;
+                msh.triangles = indices;
+                msh.RecalculateNormals();
+                msh.RecalculateBounds();
+
+                // Set up game object with mesh;
+                MeshRenderer msgr = obj.AddComponent<MeshRenderer>();
+                msgr.material = Resources.Load("building") as Material;
+                MeshFilter filter = obj.AddComponent<MeshFilter>() as MeshFilter;
+                filter.mesh = msh;
+                obj.transform.parent = this.gameObject.transform;
+                break;
+            case "Point":
+                break;
+        }
+    }
+
+    private void DrawRoads(Vector3[] vertices)
+    {
         GameObject obj = new GameObject();
-        obj.AddComponent(typeof(MeshRenderer));
-        MeshFilter filter = obj.AddComponent(typeof(MeshFilter)) as MeshFilter;
-        filter.mesh = msh;
-        obj.transform.parent = this.gameObject.transform;
-        //gameObject.GetComponent<Mesh>().SetVertices(vec3);
-        //gameObject.GetComponent<Mesh>().SetIndices(indices, MeshTopology.Triangles, 0);
-        //gameObject.GetComponent<MeshFilter>().mesh = msh;
+        LineRenderer line = obj.AddComponent<LineRenderer>();
+        //set the number of points to the line
+        line.SetVertexCount(vertices.Length);
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            line.SetPosition(i, vertices[i]);
+            setBoundaries(vertices[i].x, vertices[i].y);
+        }
+        //set the width
+        line.SetWidth(0.1f, 0.1f);
+        line.material = Resources.Load("road") as Material;
+        line.transform.parent = this.gameObject.transform;
+        line.useWorldSpace = false;
+    }
+
+    private float[] coordTrans(float lon, float lat)
+    {
+        float times = Mathf.Pow(2, zoom) / 30;
+        float worldX = (lon - longtitude) * times + 5f;
+        float worldY = (lat - latitude) * times + 5f;
+        //Debug.Log("WORLDX:" + worldX + "WORLDY:" + worldY);
+        return new float[] { worldX, worldY };
+    }
+
+    private void setBoundaries(float x, float y)
+    {
+        //上下左右
+        if (y > mapBoundaries[0])
+        {
+            mapBoundaries[0] = y;
+        }
+        if (y < mapBoundaries[1])
+        {
+            mapBoundaries[1] = y;
+        }
+        if (x < mapBoundaries[2])
+        {
+            mapBoundaries[2] = x;
+        }
+        if (x > mapBoundaries[3])
+        {
+            mapBoundaries[3] = x;
+        }
+    }
+
+    public bool GetNewTile(int[] diff)
+    {
+        xTile += diff[0];
+        yTile -= diff[1];
+        foreach (int[] tile in drewTile)
+        {
+            if (tile[0] == xTile && tile[1] == yTile)
+            {
+                Debug.Log("5515415435");
+                return false;
+            }
+        }
+        requestMap(xTile, yTile);
+        drewTile.Add(new int[] { xTile, yTile });
+        Debug.Log("XTILE:" + xTile + "YTILE:" + yTile);
+        return true;
     }
 }
